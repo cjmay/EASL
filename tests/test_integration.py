5b609f4dda79c14ce24a2fa182c2cdba1d5f91d6
Chandler May <chandler.j.may@gmail.com>
2018-06-18 12:15:47 -0400

    Test varying # HITs, # items per HIT.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,8 +1,9 @@
 from subprocess import check_call
 import os
 import shutil
+import itertools as it
 
-from pytest import fixture
+from pytest import fixture, mark
 
 from simulate_hit_results import simulate_hit_results
 
@@ -10,9 +11,7 @@ from simulate_hit_results import simulate_hit_results
 @fixture(params=['', 'test_experiments' + os.sep])
 def script_data(request):
     prefix = request.param
-    remove_dir = False
     if prefix and not os.path.isdir(prefix):
-        remove_dir = True
         os.makedirs(prefix)
     shutil.copy('experiments/political/political.csv', os.path.join(prefix, 'political.csv'))
     yield dict(prefix=prefix)
@@ -21,17 +20,27 @@ def script_data(request):
     os.remove(os.path.join(prefix, 'political_hit_1.csv'))
     os.remove(os.path.join(prefix, 'political_result_1.csv'))
     os.remove(os.path.join(prefix, 'political_1.csv'))
-    if remove_dir:
+    if prefix:
         os.rmdir(prefix)
 
 
-def test_scripts(script_data):
+@mark.parametrize('num_hits,num_items', it.product((1, 30, 900), (1, 5, 25)))
+def test_scripts(script_data, num_hits, num_items):
     prefix = script_data['prefix']
     check_call(
         'python initialize.py {prefix}political.csv'.format(prefix=prefix).split())
     check_call(
-        'python main.py --operation generate --model {prefix}political_0.csv --hits 25'.format(prefix=prefix).split())
+        'python main.py --operation generate '
+        '--model {prefix}political_0.csv --hits {num_hits} --item {num_items}'.format(
+            prefix=prefix,
+            num_hits=num_hits,
+            num_items=num_items
+        ).split())
     simulate_hit_results('{prefix}political_hit_1.csv'.format(prefix=prefix),
                          '{prefix}political_result_1.csv'.format(prefix=prefix))
     check_call(
-        'python main.py --operation update --model {prefix}political_0.csv'.format(prefix=prefix).split())
+        'python main.py --operation update '
+        '--model {prefix}political_0.csv --item {num_items}'.format(
+            prefix=prefix,
+            num_items=num_items
+        ).split())
