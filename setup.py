6b3fdd5d1bad6e43868df89cfc4838d4264e0329
Chandler May <chandler.j.may@gmail.com>
2018-07-11 08:53:53 -0400

    Move requirements to setup.py.

--- setup.py
+++ setup.py
@@ -9,4 +9,10 @@ setup(
     version='0.2dev0',
     packages=['easl'],
     scripts=glob(os.path.join('scripts', '*.py')),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'boto3',
+        'xmltodict',
+    ],
 )
