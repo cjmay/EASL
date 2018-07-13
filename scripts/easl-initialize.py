6fbe21a8877d49292e7e573bc18ad0264611f8e8
Chandler May <chandler.j.may@gmail.com>
2018-07-12 20:32:24 -0400

    Standardize command-line argument handling.

--- scripts/easl-initialize.py
+++ scripts/easl-initialize.py
@@ -2,17 +2,27 @@
 
 
 import os
-import sys
 
 from easl import EASL
 
 
-in_file_path = sys.argv[1]
-dir_path = os.path.dirname(in_file_path)
-file_name = os.path.splitext(os.path.basename(in_file_path))[0]
-out_file_name = file_name + "_0" + os.extsep + "csv"
-out_file_path = os.path.join(dir_path, out_file_name)
+if __name__ == "__main__":
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description='Read data from CSV file with at least two columns '
+                    '(id and sent) and write initial EASL model file for that '
+                    'data (replacing .csv on the input data CSV path with '
+                    '_0.csv to get the output EASL model CSV path).',
+    )
+    parser.add_argument('in_file_path', help='Path to input CSV file')
+    args = parser.parse_args()
 
-model = EASL()
-model.initItem(in_file_path)
-model.saveItem(out_file_path)
+    dir_path = os.path.dirname(args.in_file_path)
+    file_name = os.path.splitext(os.path.basename(args.in_file_path))[0]
+    out_file_name = file_name + "_0" + os.extsep + "csv"
+    out_file_path = os.path.join(dir_path, out_file_name)
+
+    model = EASL()
+    model.initItem(args.in_file_path)
+    model.saveItem(out_file_path)
--- scripts/easl-main.py
+++ scripts/easl-main.py
@@ -5,37 +5,43 @@ from easl import EASL, run
 
 
 if __name__ == "__main__":
-    import argparse
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('operation',
-                            choices=["generate", "update", "update-generate"],
-                            help="operation to run")
-    arg_parser.add_argument('model_path',
-                            help="model file path")
-    arg_parser.add_argument('--item', dest="param_items", type=int,
-                            default=EASL.DEFAULT_PARAMS['param_items'],
-                            help="number of items per hit (default 5) ")
-    arg_parser.add_argument('--match', dest="param_match", type=float,
-                            default=EASL.DEFAULT_PARAMS['param_match'],
-                            help="parameter gamma for match quality (default 0.1) ")
-    arg_parser.add_argument(
-        '--hits',
-        dest="param_hits",
-        type=int,
-        default=EASL.DEFAULT_PARAMS['param_hits'],
-        help="number of HITs to generate (recommend: hits = N/k, N=number of entire sample points, k=items per hit) ")
-    arg_parser.add_argument('--mean-windows', dest="param_mean_windows", action='store_true',
-                            help="use mean windows to compute HITs "
-                                 "(default: original EASL method)")
-    arg_parser.add_argument('--overlap', dest="param_overlap", type=int,
-                            default=EASL.DEFAULT_PARAMS['param_overlap'],
-                            help="number of items by which to overlap HITs")
-    arg_parser.add_argument('--sample-var', dest="param_sample_var", action='store_true',
-                            help="use sample variance with heuristic for 0, 1 samples "
-                                 "(default: Beta variance heuristic)")
-    arg_parser.add_argument('--na-adjust', dest="param_na_adjust", action='store_true',
-                            help="reduce variance and center mode by N/A count")
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description='Read EASL model from specified file and either '
+                    '`generate` a HIT batch CSV file from that model, '
+                    '`update` the model with HIT results (in CSV format), or '
+                    '`update-generate` (first update the model, then '
+                    'generate new HITs).',
+    )
+    parser.add_argument('operation',
+                        choices=["generate", "update", "update-generate"],
+                        help="operation to run")
+    parser.add_argument('model_path',
+                        help="model file path")
+    parser.add_argument('--item', dest="param_items", type=int,
+                        default=EASL.DEFAULT_PARAMS['param_items'],
+                        help="number of items per hit (default 5) ")
+    parser.add_argument('--match', dest="param_match", type=float,
+                        default=EASL.DEFAULT_PARAMS['param_match'],
+                        help="parameter gamma for match quality (default 0.1) ")
+    parser.add_argument('--hits', dest="param_hits", type=int,
+                        default=EASL.DEFAULT_PARAMS['param_hits'],
+                        help="number of HITs to generate (recommend: "
+                             "hits = N/k, N=number of entire sample points, "
+                             "k=items per hit)")
+    parser.add_argument('--mean-windows', dest="param_mean_windows", action='store_true',
+                        help="use mean windows to compute HITs "
+                             "(default: original EASL method)")
+    parser.add_argument('--overlap', dest="param_overlap", type=int,
+                        default=EASL.DEFAULT_PARAMS['param_overlap'],
+                        help="number of items by which to overlap HITs")
+    parser.add_argument('--sample-var', dest="param_sample_var", action='store_true',
+                        help="use sample variance with heuristic for 0, 1 samples "
+                             "(default: Beta variance heuristic)")
+    parser.add_argument('--na-adjust', dest="param_na_adjust", action='store_true',
+                        help="reduce variance and center mode by N/A count")
 
-    args = arg_parser.parse_args()
+    args = parser.parse_args()
 
     run(args.operation, args.model_path, dict(**vars(args)))
